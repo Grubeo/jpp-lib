63595fc78db5af88307ebe2c31d64fc9aedfbae5
Grubeo <robben_1@wp.pl>
2018-07-06 00:01:00 +0200

    Removing unused stuff

--- json++/functional/compound.hpp
+++ json++/functional/compound.hpp
@@ -5,22 +5,6 @@
 
 namespace fun
 {
-    template<typename Callable>
-    struct function;
-
-    template<typename Ret, typename ...Args>
-    struct function<Ret(Args...)>
-    {
-        Ret(*fn_ptr)(Args...);
-
-        Ret operator()(Args ... args) {
-            return std::invoke(fn_ptr, args...);
-        }
-    };
-
-    template<typename Ret, typename ...Args>
-    function(Ret(*)(Args...)) -> function<Ret(Args...)>;
-
     template<class... Ts> struct compound : Ts... { 
         compound(Ts && ... t) : Ts{t}... {}
 
